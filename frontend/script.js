c5fb1b971766c9186e50c591181cde692e563ea0
Nenad <sone.krivokapic@gmail.com>
2026-02-02 23:23:18 +0100

    Add task form submit handler and POST fetch request

---
+++ frontend/script.js
@@ -0,0 +1,29 @@
+document.getElementById("taskForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const taskInput = document.getElementById("taskInput");
+    const task = taskInput.value.trim();
+
+    if(!task){
+        alert("Write a task");
+        return;
+    }
+
+    const res = await fetch("http://localhost/backend/api/tasks.php", {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({ task })
+    });
+
+    const data = await res.json();
+
+    const li = document.createElement("li");
+    li.textContent = data.task;
+
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+  
+});
