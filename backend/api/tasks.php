b4a01b0eeff16451d6415431290fdd993f7580d4
Nenad <sone.krivokapic@gmail.com>
2026-02-04 17:12:01 +0100

    Implement task persistence
    - Add GET endpoint for loading tasks
    - Add GET logic to frontend
    - Update POST frontend logic to use loadTasks insted of manual append

--- backend/api/tasks.php
+++ backend/api/tasks.php
@@ -4,27 +4,42 @@ header("Content-Type: application/json");
 
 require_once "../config/db.php";
 
-//Uzmi json od frontend-a
-$data = json_decode(file_get_contents("php://input"), true);
+//Ako je methoda requesta GET - uzima sve taskove iz baze
+if($_SERVER["REQUEST_METHOD"] === "GET"){
 
-//Proveri da li postoji task
-if(!empty($data["task"])){
-    $task = trim($data["task"]);
+    //Uzme taskove iz baze
+    $stmt = $pdo->query("SELECT * FROM tasks ORDER BY id DESC");
+    $task = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    //Insert taska u bazu
-    $stmt = $pdo->prepare("INSERT INTO tasks (task) VALUES (:task)");
-    $stmt->execute([
-        "task" => $task
-    ]);
+    //Vrati frontendu
+    echo json_encode($task);
+    exit;
+}
 
-    //Vrati nazad frontend-u
-    echo json_encode([
-        "task" => $task
-    ]);
+//Ako je methoda requesta POST - upisuje taskove u bazu
+if($_SERVER["REQUEST_METHOD"] === "POST"){
 
-}else {
-    echo json_encode([
-        "error" => "Task is empty"
-    ]);
+    //Uzme json od frontend-a
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    //Proveri da li postoji task
+    if(!empty($data["task"])){
+        $task = trim($data["task"]);
+
+        //Insert taska u bazu
+        $stmt = $pdo->prepare("INSERT INTO tasks (task) VALUES (:task)");
+        $stmt->execute([
+            "task" => $task
+        ]);
+
+        //Vrati nazad frontend-u
+        echo json_encode([
+            "task" => $task
+        ]);
+    }else {
+        echo json_encode([
+            "error" => "Task is empty"
+        ]);
+    }
 }
 ?>
--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,25 @@
+//GET - za uzimanje taskova iz backenda
+window.addEventListener("DOMContentLoaded", loadTasks());
+
+async function loadTasks() {
+
+    const res = await fetch("http://localhost:8000/backend/api/tasks.php");
+    const tasks = await res.json();
+
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+        li.textContent = task.task;
+        list.appendChild(li);
+    });
+    
+}
+
+//POST - za slanje taskova backendu
 document.getElementById("taskForm").addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
     const taskInput = document.getElementById("taskInput");
@@ -17,13 +38,8 @@ document.getElementById("taskForm").addEventListener("submit", async (e) => {
         body: JSON.stringify({ task })
     });
 
-    const data = await res.json();
-
-    const li = document.createElement("li");
-    li.textContent = data.task;
-
-    document.getElementById("taskList").appendChild(li);
-
+    await res.json();
+    await loadTasks();
     taskInput.value = "";
   
 });
