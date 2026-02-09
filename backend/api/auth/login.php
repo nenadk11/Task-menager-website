eee8d1d7cd2b81c720247107dbc96e4c17792614
Nenad <sone.krivokapic@gmail.com>
2026-02-09 19:28:15 +0100

    Implement user login frontend and backend

---
+++ backend/api/auth/login.php
@@ -0,0 +1,41 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+require_once __DIR__ . '/../../config/db.php';
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$email = trim($data["email"] ?? "");
+$password = $data["password"] ?? "";
+
+//Provera da li je email ili sifra prazna
+if(empty($email) || empty($password)){
+    echo json_encode(["success" => false, "message" => "Email and password required"]);
+    exit;
+}
+
+try {
+
+    $stmt = $pdo->prepare("SELECT id, username, email, password FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($user && password_verify($password, $user["password"])){
+        echo json_encode([
+            "success" => true,
+            "user" => [
+                "id" => $user["id"],
+                "username" => $user["username"],
+                "email" => $user["email"]
+                ]
+            ]);
+    }else{
+        echo json_encode(["success" => false, "message" => "Invalid email or password"]);
+    }
+
+}catch(PDOException $e) {
+    echo json_encode(["success" => false, "message" => "Server error"]);
+}
+?>
---
+++ frontend/js/login.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = form.elements["email"].value.trim();
+    const password = form.elements["password"].value;
+
+    if(!email || !password){
+        alert("All fields are required");
+        return;
+    }
+
+    //Slanje inputa backendu
+    try {
+        const res = await fetch("http://localhost:8000/backend/api/auth/login.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        const data = await res.json();
+
+        if(data.success){
+            alert("Login successful");
+            window.location.href = "index.html";
+        }else{
+            alert(data.message || "Login unsuccessful");
+        }
+    }catch (err) {
+        console.error(err);
+        alert("Something went wrong. Please try again.");
+    }
+});
